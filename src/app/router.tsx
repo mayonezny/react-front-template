bdf79665285c85b60661bf761231f84500cb6870
mayonezny <borischubuhchiev@mail.ru>
2025-10-20 15:15:46 +0300

    обновил теймплейт: 1. Husky подключен и работает как надо. 2. Добавлен CSS-normalize и Reset, добавлена базовая цветовая палитра (оранж) и базовые переменные для шрифтов и отступов 3. Настройки эдитора VS Code больше не в gitignore. 4. Prettier теперь требует кареток CRLF (для совместимости с гитхабом) 5. По дефолту @ резолвится как алиас "./src" 6. Добавлены недостающие для архитектуры FSD папки 7. Добавлен компонент Wrapper для несменяющихся элементов (хедеров, футеров, сайдбаров и всего такого) и засунут в роутер 8.  Папка с редаксом перенесена в нужное место

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { Wrapper } from '@/components/Wrapper';
+
 import { HomePage } from '../pages/HomePage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />, // общая обёртка с меню
+    element: <Wrapper />, // общая обёртка с меню
     children: [
-      // { index: true, element: <HomePage /> },
+      { index: true, element: <HomePage /> },
       // { path: 'todos', element: <TodosPage /> },
     ],
   },
